e6ff5e4fde06d0a81ba4e39f8e66e94faf976e36
Shubhangam333 <shubhangam27@gmail.com>
2024-07-17 15:06:24 +0530

    create avatarview for profilepage

--- client/app/views/Profile.tsx
+++ client/app/views/Profile.tsx
@@ -1,18 +1,51 @@
+import AvatarView from "@ui/AvatarView";
+import colors from "@utils/colors";
+import size from "@utils/size";
+import useAuth from "app/hooks/useAuth";
 import { FC } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
 
 interface Props {}
 
 const Profile: FC<Props> = (props) => {
+  const { authState } = useAuth();
+  const { profile } = authState;
+
   return (
-    <View style={styles.container}>
-      <Text>Profile</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.profileContainer}>
+        <AvatarView uri={profile?.avatar} size={80} />
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>{profile?.name}</Text>
+          <Text style={styles.email}>{profile?.email}</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    padding: size.padding,
+  },
+  profileContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  name: {
+    color: colors.primary,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  email: {
+    color: colors.primary,
+    paddingTop: 2,
+  },
+  profileInfo: {
+    flex: 1,
+    paddingLeft: size.padding,
+  },
 });
 
 export default Profile;
